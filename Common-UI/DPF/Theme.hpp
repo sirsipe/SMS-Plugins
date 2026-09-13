22ee818823a8a92fd508d1516bd9dff9f18b700f
SirSipe <simo.erkinheimo@gmail.com>
2026-09-13 18:31:13 +0000

    Adopt graphite red UI theme

--- Common-UI/DPF/Theme.hpp
+++ Common-UI/DPF/Theme.hpp
@@ -5,17 +5,17 @@
 namespace sms::ui::dpf {
 
 struct Theme {
-    DGL_NAMESPACE::Color canvas{14, 17, 24};
-    DGL_NAMESPACE::Color surface{22, 27, 37};
-    DGL_NAMESPACE::Color surfaceRaised{29, 35, 47};
-    DGL_NAMESPACE::Color outline{51, 61, 78};
-    DGL_NAMESPACE::Color contentPrimary{232, 238, 247};
-    DGL_NAMESPACE::Color contentSecondary{143, 155, 177};
-    DGL_NAMESPACE::Color activityPlayback{65, 214, 203};
-    DGL_NAMESPACE::Color activityCapture{255, 175, 84};
-    DGL_NAMESPACE::Color selection{255, 175, 84};
-    DGL_NAMESPACE::Color controlAccent{255, 175, 84};
-    DGL_NAMESPACE::Color intentDanger{238, 101, 112};
+    DGL_NAMESPACE::Color canvas{15, 15, 18};
+    DGL_NAMESPACE::Color surface{27, 27, 33};
+    DGL_NAMESPACE::Color surfaceRaised{39, 39, 47};
+    DGL_NAMESPACE::Color outline{69, 68, 81};
+    DGL_NAMESPACE::Color contentPrimary{244, 241, 246};
+    DGL_NAMESPACE::Color contentSecondary{165, 158, 174};
+    DGL_NAMESPACE::Color activityPlayback{232, 55, 68};
+    DGL_NAMESPACE::Color activityCapture{255, 94, 77};
+    DGL_NAMESPACE::Color selection{255, 74, 91};
+    DGL_NAMESPACE::Color controlAccent{232, 55, 68};
+    DGL_NAMESPACE::Color intentDanger{255, 30, 55};
     DGL_NAMESPACE::Color meterGreen{75, 211, 112};
     DGL_NAMESPACE::Color meterYellow{245, 198, 66};
     DGL_NAMESPACE::Color meterRed{239, 83, 96};
